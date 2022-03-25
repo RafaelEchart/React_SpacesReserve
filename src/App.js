420b01efa9ffdb6bfd75f8d6fa2efa1a774e7c58
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-03-25 19:14:53 +0500

    Get redux state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { actionCreator } from './store/task';
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(actionCreator());
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Hello React App</h2>
+        <h1>Hello React App - {state}</h1>
       </header>
     </div>
   );
