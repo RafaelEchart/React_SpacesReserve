a997d97e0ab25658e779376ecd9fad7fb7a1f73c
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-03-25 19:13:12 +0500

    Setup ducks pattern

---
+++ src/store/task.js
@@ -0,0 +1,18 @@
+// action types
+const ACTION_TYPE = 'ProjectName/ReducerName/ActionType';
+
+// action creators
+export const actionCreator = () => ({ type: ACTION_TYPE, payload: 'Payload' });
+
+// initial state
+const initialState = 'Initial State';
+
+// reducers
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTION_TYPE:
+      return [...state];
+  }
+};
+
+export default reducer;
