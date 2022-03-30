06d3e02869c63146a4415f07edabca83788b1e07
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-03-30 15:11:16 -0500

    Input handler for log in

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,36 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
-const LoginForm = () => (
-  <>
-    <div className="login_container">
-      <div className="session">
-        <div className="left" />
-        <form action="" className="log-in" autoComplete="off">
-          <h4 className="login_title">
-            We are
-            <span className="login_span">SPACES</span>
-          </h4>
-          <p className="login_p">Welcome back! Log into your account and check the best space for your company!</p>
-          <div className="floating-label">
-            <input placeholder="Email" className="login_input" type="email" name="email" id="email" autoComplete="off" />
-            <div className="icon">
-              <ion-icon name="mail-outline" class="email_icon" />
+const LoginForm = () => {
+  const [loginData, setLoginData] = useState({ email: '', password: '' });
+
+  const formDataHandler = (e, field) => {
+    const input = e.target.value;
+    switch (field) {
+      case 'email':
+        setLoginData({ ...loginData, email: input });
+        break;
+
+      case 'password':
+        setLoginData({ ...loginData, password: input });
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const submitLogin = () => {
+    // if (loginData.email.length && loginData.password.length) {
+
+    // } else {
+
+    // }
+  };
+
+  return (
+    <>
+      <div className="login_container">
+        <div className="session">
+          <div className="left" />
+          <div action="" className="log-in" autoComplete="off">
+            <h4 className="login_title">
+              We are
+              <span className="login_span">SPACES</span>
+            </h4>
+            <p className="login_p">Welcome back! Log into your account and check the best space for your company!</p>
+            <div className="floating-label">
+              <input placeholder="Email" className="login_input" type="email" name="email" onChange={(e) => formDataHandler(e, 'email')} id="email" autoComplete="off" />
+              <div className="icon">
+                <ion-icon name="mail-outline" class="email_icon" />
+
+              </div>
+            </div>
+            <div className="floating-label">
+              <input placeholder="Password" className="login_input" type="password" name="password" onChange={(e) => formDataHandler(e, 'password')} id="password" autoComplete="off" />
+              <div className="icon">
+                <ion-icon name="key-outline" class="email_icon" />
+              </div>
 
             </div>
+            <button type="submit" onClick={submitLogin} className="login_button">Log in</button>
           </div>
-          <div className="floating-label">
-            <input placeholder="Password" className="login_input" type="password" name="password" id="password" autoComplete="off" />
-            <div className="icon">
-              <ion-icon name="key-outline" class="email_icon" />
-            </div>
-
-          </div>
-          <button type="submit" onClick="return false;" className="login_button">Log in</button>
-        </form>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default LoginForm;
