250c9559d698e47d9847a081eea07cf1d878b38d
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-04-01 04:55:30 +0500

    Add reset input values function

--- src/components/Spaces/form.js
+++ src/components/Spaces/form.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-param-reassign */
 import React, { useState } from 'react';
+import { message } from 'antd';
 
 const Form = () => {
   const [spaceData, setSpaceData] = useState({
@@ -32,8 +34,36 @@ const Form = () => {
     }
   };
 
-  const handleSubmisson = () => {
-    console.log(spaceData);
+  const resetInputs = () => {
+    const fields = document.querySelectorAll('.login_input');
+    fields.forEach((field) => {
+      field.value = '';
+    });
+  };
+
+  const handleSubmisson = async () => {
+    const { token } = JSON.parse(localStorage.getItem('userInformation'));
+    try {
+      fetch('http://localhost:3000/spaces', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token,
+        },
+        body: JSON.stringify({
+          space: {
+            name: spaceData.name,
+            description: spaceData.description,
+            price: spaceData.price,
+            image: spaceData.image,
+          },
+        }),
+      });
+      message.success('New space added successfully');
+      resetInputs();
+    } catch (error) {
+      message.error(error);
+    }
   };
 
   return (
