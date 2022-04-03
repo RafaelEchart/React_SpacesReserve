ae262244acc3f1d043e3cb2016e4871aee2fa1f1
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-04-03 23:02:38 +0500

    Update path for details page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import SpaceForm from './components/Spaces/form';
 import DrawerMenu from './components/DrawerMenu';
 import './assets/stylesheets/App.css';
@@ -15,6 +11,7 @@ import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
 import Spaces from './components/Spaces/index';
 import Details from './components/Spaces/Details';
+import ReservationForm from './components/Spaces/ReservationForm';
 
 function App() {
   const dispatch = useDispatch();
@@ -27,22 +24,23 @@ function App() {
 
   return (
     <div className="App">
-      <DrawerMenu
-        logedIn={userInformation}
-        admin={admin}
-      />
+      <DrawerMenu logedIn={userInformation} admin={admin} />
       <Routes>
         <>
-          <Route path="/" element={userInformation ? <HomePageWithSession /> : <HomePageNoSession />} />
-          {admin
-            ? (
-              <>
-                <Route path="/new_space" element={<SpaceForm />} />
-                <Route path="/spaces" element={<Spaces />} />
-                <Route path="/space/:id" element={<Details />} />
-              </>
-            )
-            : null}
+          <Route
+            path="/"
+            element={
+              userInformation ? <HomePageWithSession /> : <HomePageNoSession />
+            }
+          />
+          {admin ? (
+            <>
+              <Route path="/new_space" element={<SpaceForm />} />
+              <Route path="/spaces" element={<Spaces />} />
+              <Route path="/spaces/:details" element={<Details />} />
+              <Route path="spaces/:details/new_reservation" element={<ReservationForm />} />
+            </>
+          ) : null}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/*" element={<Navigate to="/" />} />
