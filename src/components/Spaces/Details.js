76456bce051847360d6b7e326075021cf771c290
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-04-08 16:45:02 +0500

    Create a separate component for details page image

--- src/components/Spaces/Details.js
+++ src/components/Spaces/Details.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { message } from 'antd';
-import {
-  CaretLeftOutlined,
-  RightCircleOutlined,
-} from '@ant-design/icons';
+import { CaretLeftOutlined, RightCircleOutlined } from '@ant-design/icons';
 import SpinLoading from '../Spinner';
+import DetailsImage from './DetailsImage';
 
 const Details = () => {
   const { id } = useParams();
@@ -16,9 +14,12 @@ const Details = () => {
     try {
       setIsLoading(true);
       const { token } = JSON.parse(localStorage.getItem('userInformation'));
-      const response = await fetch(`https://api-spaces.herokuapp.com/spaces/${id}`, {
-        headers: { Authorization: token },
-      });
+      const response = await fetch(
+        `https://api-spaces.herokuapp.com/spaces/${id}`,
+        {
+          headers: { Authorization: token },
+        }
+      );
       const data = await response.json();
       setSpace(data);
       setIsLoading(false);
@@ -31,58 +32,65 @@ const Details = () => {
     getSpace();
   }, []);
   return (
-    <>
-      {isLoading ? <div className="center_spinner"><SpinLoading /></div> : null}
+    <section>
+      {isLoading ? (
+        <div className="center_spinner">
+          <SpinLoading />
+        </div>
+      ) : null}
 
-      {space && !isLoading
-        ? (
-          <section className="details-page">
-            <div className="details-section">
+      {space && !isLoading ? (
+        <section className="details-page">
+          <div className="details-section">
+            <div>
+              <DetailsImage
+                width="600"
+                height="600"
+                src={space.image}
+                className="details_image"
+                alt="Space Details"
+              />
+            </div>
+            <div className="space-details">
+              <div className="space-main-details">
+                <p className="space-name">{space.name}</p>
+                <p className="space-desc">{space.description}</p>
+              </div>
               <div>
-                <img width={600} height={600} src={space.image} className="details_image" alt="Space Details" />
-              </div>
-              <div className="space-details">
-                <div className="space-main-details">
-                  <p className="space-name">{space.name}</p>
-                  <p className="space-desc">{space.description}</p>
-                </div>
-                <div>
-
-                  <ul className="details">
-                    <li className="details-row span_space_details">
-                      <span>Price</span>
-                      <span>
-                        $
-                        {space.price}
-                      </span>
-                    </li>
-                    <li className="details-row">
-                      <span>Created:</span>
-                      <span>{space.created_at.split('T')[0]}</span>
-                    </li>
-                    <li className="details-row span_space_details">
-                      <span>ID:</span>
-                      <span>{space.id}</span>
-                    </li>
-                  </ul>
-                  <Link key="link1" to={`/spaces/${id}/reservation`}>
-                    <div className="reserve-btn">
-                      <span>Reserve</span>
-                      <RightCircleOutlined />
-                    </div>
-                  </Link>
-                </div>
+                <ul className="details">
+                  <li className="details-row span_space_details">
+                    <span>Price</span>
+                    <span>${space.price}</span>
+                  </li>
+                  <li className="details-row">
+                    <span>Created:</span>
+                    <span>{space.created_at.split('T')[0]}</span>
+                  </li>
+                  <li className="details-row span_space_details">
+                    <span>ID:</span>
+                    <span>{space.id}</span>
+                  </li>
+                </ul>
+                <Link key="link1" to={`/spaces/${id}/reservation`}>
+                  <div className="reserve-btn">
+                    <span>Reserve</span>
+                    <RightCircleOutlined />
+                  </div>
+                </Link>
               </div>
             </div>
-            <div className="back-btn">
-              <Link key="link" to="/">
-                <CaretLeftOutlined style={{ color: '#fff' }} className="back-icon" />
-              </Link>
-            </div>
-          </section>
-        )
-        : null}
-    </>
+          </div>
+          <div className="back-btn">
+            <Link key="link" to="/">
+              <CaretLeftOutlined
+                style={{ color: '#fff' }}
+                className="back-icon"
+              />
+            </Link>
+          </div>
+        </section>
+      ) : null}
+    </section>
   );
 };
 
